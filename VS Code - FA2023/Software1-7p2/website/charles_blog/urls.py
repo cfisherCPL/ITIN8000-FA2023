197aa53b85d5323c3ad1ffec46e56ee4c170599c
CharlesMakesGames <cfisher@unomaha.edu>
2023-09-24 20:55:24 -0500

    cat_pics
    Error in viewing cat_pics page after testing server. Error status claims that cat_pics is not in templates, but Debug page shows that it is using 3.11 python as interpreter. Possible interp issue. Again.

--- VS Code - FA2023/Software1-7p2/website/charles_blog/urls.py
+++ VS Code - FA2023/Software1-7p2/website/charles_blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-            	path('home/', views.home, name='home'),
+    path('home/', views.home, name='home'),
+	path('cat_pics/', views.cat_pics, name='cat_pics'),
 ]
--- VS Code - FA2023/Software1-7p2/website/charles_blog/views.py
+++ VS Code - FA2023/Software1-7p2/website/charles_blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home (request):
     return render (request, "home.html")
+
+def cat_pics (request):
+    return render (request, "cat_pics")
