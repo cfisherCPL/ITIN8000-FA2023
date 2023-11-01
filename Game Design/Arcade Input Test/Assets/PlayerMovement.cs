0b4d7319f9e5ac1ec3320faf20e3741a7e5ac79a
CharlesMakesGames <cfisher@unomaha.edu>
2023-10-31 19:10:46 -0500

    Arcade Input Test
    Testing usb encoder input for digital arcade deck to unity.

--- Game Design/Arcade Input Test/Assets/PlayerMovement.cs
+++ Game Design/Arcade Input Test/Assets/PlayerMovement.cs
@@ -42,5 +42,66 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+
+        //button testing to log
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            Debug.Log("Button 0 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            Debug.Log("Button 1 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+        {
+            Debug.Log("Button 2 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3))
+        {
+            Debug.Log("Button 3 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button4))
+        {
+            Debug.Log("Button 4 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button5))
+        {
+            Debug.Log("Button 5 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button6))
+        {
+            Debug.Log("Button 6 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            Debug.Log("Button 6 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button8))
+        {
+            Debug.Log("Button 8 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button9))
+        {
+            Debug.Log("Button 9 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button10))
+        {
+            Debug.Log("Button 10 Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button11))
+        {
+            Debug.Log("Button 11 Pressed");
+        }
     }
 }
